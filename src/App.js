deea87dc7a3fe844f0e5215d7d61549e80cc0a87
mahmoudhamdy123 <m.hamdy@spiritude.co>
2023-04-01 00:27:00 +0200

    Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import "./App.css";
+
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div>
-      <ExpenseItem></ExpenseItem>
+      <h2>Let's get started</h2>
+      <Expenses  items = {expenses}/>
+    
     </div>
   );
 }
 
 export default App;
 
-
-  // const para = document.createElement('p');
-  // para.textContent = "Hi mahmoud";
-  // document.getElementById('root').append(para);
-
-  
+// const para = document.createElement('p');
+// para.textContent = "Hi mahmoud";
+// document.getElementById('root').append(para);
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,17 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card"
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
   return (
-    <div className='expense-item'>
-      <div>March 26th 2023</div>
-      <div className='expense-item__description'>
-              <h2>Car Insurance</h2>
-              <div className='expense-item__price'>$123</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
