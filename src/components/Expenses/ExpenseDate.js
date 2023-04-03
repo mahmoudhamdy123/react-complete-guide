dfbf21d3adac1da5d3276c077d3eb29eb5685417
mahmoudhamdy123 <m.hamdy@spiritude.co>
2023-04-03 10:52:59 +0200

    Update state based on pervies state

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -7,7 +7,7 @@ const ExpenseDate = (props) => {
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     return (
         <div className="expense-date">
-            <div class  Name="expense-date__month"> {month}</div>
+            <div className="expense-date__month"> {month}</div>
             <div className="expense-date__day">{day}</div>
             <div className="expense-date__year">{year}</div>
         </div>
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,26 +1,66 @@
-import React from 'react'; 
-import './ExpenseForm.css';
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    return <form>
-        <div className='new-expense__controls'>
-            <div className='new-expense__controls'>
-                <label>Title</label>
-                <input type='text'/>
-            </div>
-            <div className='new-expense__controls'>
-                <label>Amount</label>
-                <input type='number' min='0.01' step='0.01 '/>
-            </div>
-            <div className='new-expense__controls'>
-                <label>Date</label>
-                <input type='date' min='2019-01-01' max='2022-12-31'/>
-            </div>
+  //   const [title, setTitle] = useState("");
+  //   const [amount, setAmount] = useState("");
+  //   const [date, setDate] = useState("");
+
+  const [userInput, setUserInput] = useState({
+    title: "",
+    amount: "",
+    date: "",
+  });
+
+  const titleChangeHandler = (event) => {
+      setUserInput((prevState) => {
+          return {...prevState,title:event.target.value };
+      });
+  };
+
+  const amountChangeHandler = (event) => {
+    setUserInput((prevState) => {
+        return {...prevState,amount:event.target.value };
+    });
+  };
+
+  const dateChangeHandler = (event) => {
+    setUserInput((prevState) => {
+        return {...prevState,date:event.target.value };
+    });
+  };
+
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__controls">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
         </div>
-        <div className='new-expense__actions'>
-            <button>Add Expense </button>
+        <div className="new-expense__controls">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01 "
+            onChange={amountChangeHandler}
+          />
         </div>
-    </form>;
+        <div className="new-expense__controls">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button>Add Expense </button>
+      </div>
+    </form>
+  );
 };
 
-export default ExpenseForm
+export default ExpenseForm;
